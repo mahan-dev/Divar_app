2232e8dddcf9422feb72a3290d07b2e53591aac6
mahan.dev <mahan.dev.front@gmail.com>
2025-01-14 00:03:15 +0330

    adding vercel config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://mahan-dev.github.io/divar_site"
+  base: "https://mahan-dev.github.io/Divar_app"
 })
